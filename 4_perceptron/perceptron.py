f2258d1ad3c0ac111e8dad709445518fa2136664
Alexandre Vilarinho <33914918+alexandrecvf@users.noreply.github.com>
2019-10-18 10:47:39 -0300

    Perceptron Finalizado

--- 4_perceptron/perceptron.py
+++ 4_perceptron/perceptron.py
@@ -7,18 +7,18 @@ class Perceptron:
         w = [0, 0, 0]
         a = 1
         limiar = 0
+        epoca = 0
 
-        for k in range(0, 2):
+        while (True):
+            peso1 = w[0]
+            peso2 = w[1]
+            bias = w[2]
+
             for i in range(0, len(inputs)):
                 yin = 0
                 for j in range(0, len(inputs[i])):
                     yin += inputs[i][j] * w[j]
                 yin += w[2]
-
-                peso1 = w[0]
-                peso2 = w[1]
-
-                print(yin)
 
                 if yin > limiar:
                     y = 1
@@ -32,16 +32,31 @@ class Perceptron:
                     w[1] = w[1] + a * targets[i] * inputs[i][1]
                     w[2] = w[2] + a * targets[i]
 
-                print(w)
+            epoca = epoca + 1
 
-                if peso1 == w[0] and peso2 == w[1]:
-                    break
-        return w
+            if peso1 == w[0] and peso2 == w[1] and bias == w[2]:
+                break
+        return w, epoca
 
 
 if __name__ == '__main__':
     h = Perceptron
-    inputs = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+    inputs = [[1, 1], [1, 0], [0, 1], [0, 0]]
     targets = [1, -1, -1, -1]
 
-    print("Weights: ", h.learn(0, inputs, targets))
+    (w, epoca) = h.learn(0, inputs, targets)
+
+    print("PERCEPTRON")
+
+    print("\nInputs", "\t", "Targets")
+    for i in range(len(inputs)):
+        print(inputs[i], "\t", targets[i])
+
+    print("\nPesos: ")
+    for i in range(len(w)-1):
+        strw = "w" + str(i)
+        print("\t", strw, "=", w[i])
+
+    print("\t", "wb", "=", w[2])
+
+    print("epocas: ", epoca)
